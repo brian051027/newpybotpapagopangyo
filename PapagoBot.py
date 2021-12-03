c414b0c55236f4a85dfee7b091c284337c9a5928
brian051027 <70101429+brian051027@users.noreply.github.com>
2021-12-04 01:27:53 +0900

    Update PapagoBot.py

--- PapagoBot.py
+++ PapagoBot.py
@@ -30,7 +30,7 @@ client_secret = "MJkS7xnRud"
 client = discord.Client()
 @client.event # Use these decorator to register an event.
 async def on_ready(): # on_ready() event : when the bot has finised logging in and setting things up
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("!도움말을 ."))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("번역기 가동중!"))
     print("New log in as {0.user}".format(client))
 
 @client.event
@@ -327,18 +327,7 @@ async def on_message(message): # on_message() event : when the bot has recieved 
                 # Simplified Chinese
                 dataParmas = "source=zh-CN&target=ko&text=" + combineword
 
-    if message.content == "!도움말":
-        embed = discord.Embed(title="도움말", description="번역 안내", color=0x62c1cc)
-        embed.add_field(name="한영번역", value="!한영번역 (내용)", inline=False)
-        embed.add_field(name="영한번역", value="!영한번역 (내용)", inline=False)
-        embed.add_field(name="한일번역", value="!한일번역 (내용)", inline=False)
-        embed.add_field(name="일한번역", value="!일한번역 (내용)", inline=False)
-        embed.add_field(name="한중번역", value="!한중번역 (내용)", inline=False)
-        embed.add_field(name="중한번역", value="!중한번역 (내용)", inline=False)
-        embed.set_footer(text="by 설준서")
-        await message.channel.send("도움말 입니다.", embed=embed)
-        
-                
+
                 # Make a Request Instance
                 request = Request(baseurl)
                 # add header to packet
@@ -368,5 +357,4 @@ async def on_message(message): # on_message() event : when the bot has recieved 
             await message.channel.send("Translate Failed. HTTPError Occured.")
 access_token = os.environ["BOT_TOKEN"]
 client.run(access_token)
-
 
